6f31b2cb5302f47d0b3a965bff3b19d8b17a3c1a
John <ebusd@ebusd.eu>
2023-10-13 22:54:13 +0200

    gen index

--- .github/workflows/genindex.sh
+++ .github/workflows/genindex.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cd "$1" && (ls *.csv|tr -d "\r"|tr "\n" "#"|sed -e 's/#$//' -e 's/#/\\n/g' -e 's#^#"#' -e 's#$#"#') > index.json
+cd "$1" || exit 1
+for file in `find . -maxdepth 1 -type f`; do
+  t=`git log -m -r --name-only --no-color --pretty=raw -z -1 "$file"|grep -a committer|head -n 1|sed -e 's# +[0-9]*$##' -e 's#^.* ##'`
+  if [[ $t -gt 1 ]]; then
+    touch -d "@$t" -c -m "$file"
+  fi
+done
+ls *.csv|tr -d "\r"|tr "\n" "#"|sed -e 's/#$//' -e 's/#/\\n/g' -e 's#^#"#' -e 's#$#"#' > index.json
