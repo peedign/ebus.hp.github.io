3df281787a4e23dbf2515d311f918856a35294ec
John <ebusd@ebusd.eu>
2024-07-06 19:48:02 +0200

    switch to rework branch and generate from typespec, add mapping gen, skip push during tests

---
+++ .github/workflows/genmapping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mapfile=$(realpath "$1")
+base=$(realpath "$2/$3")
+cd "$base" || exit 1
+mf=$(basename "$3")
+indent=
+path=
+if [[ "x$mf" == "x." ]]; then
+  mf=""
+else
+  echo " - $mf" >> $mapfile
+  indent="  "
+  path="$mf/"
+fi
+
+pwd
+echo $mf
+echo $base
+
+find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "_templates|main\.tsp|\.yaml|_inc\.tsp"|egrep "\.tsp"|sort|sed -e "s#^\(.*\)\.tsp\$#$indent - [\1.tsp](https://github.com/john30/ebusd-configuration/tree/master/src/$path\1.tsp) \&rarr; [\1.csv](https://ebus.github.io/cfg/$path\1.csv)#" >> $mapfile
+find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "main\.tsp|\.yaml"|egrep "_templates|_inc\.tsp"|sort|sed -e "s#^\(.*\)\$#$indent - [\1](https://github.com/john30/ebusd-configuration/tree/master/src/$path\1) \&rarr; inlined in other file(s)#" >> $mapfile
