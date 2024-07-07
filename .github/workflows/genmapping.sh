b95e655ec82dfe50234b162ea26a6e11f52863a9
John <ebusd@ebusd.eu>
2024-07-07 11:11:40 +0200

    adjust to new location, use relative paths

--- .github/workflows/genmapping.sh
+++ .github/workflows/genmapping.sh
@@ -8,14 +8,10 @@ path=
 if [[ "x$mf" == "x." ]]; then
   mf=""
 else
-  echo " - $mf" >> $mapfile
   indent="  "
   path="$mf/"
+  echo " - [$mf](https://github.com/john30/ebusd-configuration/tree/master/src/$path)" >> $mapfile
 fi
 
-pwd
-echo $mf
-echo $base
-
-find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "_templates|main\.tsp|\.yaml|_inc\.tsp"|egrep "\.tsp"|sort|sed -e "s#^\(.*\)\.tsp\$#$indent - [\1.tsp](https://github.com/john30/ebusd-configuration/tree/master/src/$path\1.tsp) \&rarr; [en: \1.csv](https://ebus.github.io/cfg/en/$path\1.csv) / [de: \1.csv](https://ebus.github.io/cfg/de/$path\1.csv)#" >> $mapfile
+find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "_templates|main\.tsp|\.yaml|_inc\.tsp"|egrep "\.tsp"|sort|sed -e "s#^\(.*\)\.tsp\$#$indent - [\1.tsp](https://github.com/john30/ebusd-configuration/tree/master/src/$path\1.tsp) \&rarr; [en: \1.csv](en/$path\1.csv) / [de: \1.csv](de/$path\1.csv)#" >> $mapfile
 find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "main\.tsp|\.yaml"|egrep "_templates|_inc\.tsp"|sort|sed -e "s#^\(.*\)\$#$indent - [\1](https://github.com/john30/ebusd-configuration/tree/master/src/$path\1) \&rarr; inlined in other file(s)#" >> $mapfile
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Description
-This is a service providing configuration files for [ebusd](https://github.com/john30/ebusd) on [a github pages](https://ebus.github.io/cfg/) and/or CDN basis to address the increasing demand for those.
+This is a service providing configuration files for [ebusd](https://github.com/john30/ebusd) on [a github pages](https://ebus.github.io/) and/or CDN basis to address the increasing demand for those.
 
 The CSV files currently consumed by [ebusd](https://github.com/john30/ebusd) are generated from TypeSpec source files found in the [`src` folder of ebusd-configuration](https://github.com/john30/ebusd-configuration/tree/master/src) and converted to CSV using the [eBUS TypeSpec library](https://github.com/john30/ebus-typespec). The conversion process is described there in detail.
 
