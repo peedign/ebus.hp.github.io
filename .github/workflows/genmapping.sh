b7fd67c3d28752a168a686e46884a4f2c7ce600b
John <ebusd@ebusd.eu>
2024-07-07 15:49:42 +0200

    include symlinks as well

--- .github/workflows/genmapping.sh
+++ .github/workflows/genmapping.sh
@@ -14,5 +14,5 @@ else
   echo " - [$mf](https://github.com/john30/ebusd-configuration/tree/$branch/src/$path)" >> $mapfile
 fi
 
-find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "_templates|main\.tsp|\.yaml|_inc\.tsp"|egrep "\.tsp"|sort|sed -e "s#^\(.*\)\.tsp\$#$indent - [\1.tsp](https://github.com/john30/ebusd-configuration/tree/$branch/src/$path\1.tsp) \&rarr; [en: \1.csv](en/$path\1.csv) / [de: \1.csv](de/$path\1.csv)#" >> $mapfile
-find . -maxdepth 1 -type f -exec basename \{\} \;|egrep -v "main\.tsp|\.yaml"|egrep "_templates|_inc\.tsp"|sort|sed -e "s#^\(.*\)\$#$indent - [\1](https://github.com/john30/ebusd-configuration/tree/$branch/src/$path\1) \&rarr; inlined in other file(s)#" >> $mapfile
+find . -maxdepth 1 -not -type d -name "*.tsp" -exec basename \{\} \;|egrep -v "_templates|main\.tsp|\.yaml|_inc\.tsp"|sort|sed -e "s#^\(.*\)\.tsp\$#$indent - [\1.tsp](https://github.com/john30/ebusd-configuration/tree/$branch/src/$path\1.tsp) \&rarr; [en: \1.csv](en/$path\1.csv) / [de: \1.csv](de/$path\1.csv)#" >> $mapfile
+find . -maxdepth 1 -not -type d -name "*.tsp" -exec basename \{\} \;|egrep -v "main\.tsp|\.yaml"|egrep "_templates|_inc\.tsp"|sort|sed -e "s#^\(.*\)\$#$indent - [\1](https://github.com/john30/ebusd-configuration/tree/$branch/src/$path\1) \&rarr; inlined in other file(s)#" >> $mapfile
