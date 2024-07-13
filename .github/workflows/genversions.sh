e80b8c0d07a0705168c6e3ab95cc196b4d5d5bb6
John <ebusd@ebusd.eu>
2024-07-13 09:48:07 +0200

    sort for better stability

--- .github/workflows/genversions.sh
+++ .github/workflows/genversions.sh
@@ -3,4 +3,4 @@ dir=$(dirname "$0")
 dir=$(realpath "$dir")
 srcdir=$(realpath "$1")
 
-(cd "$1" && find . -name "*.csv" | xargs node $dir/genversions.mjs -i "$srcdir/versions.json") > "$1/versions.json.new" && mv "$1/versions.json.new" "$1/versions.json"
+(cd "$1" && find . -name "*.csv" | sort | xargs node $dir/genversions.mjs -i "$srcdir/versions.json") > "$1/versions.json.new" && mv "$1/versions.json.new" "$1/versions.json"
